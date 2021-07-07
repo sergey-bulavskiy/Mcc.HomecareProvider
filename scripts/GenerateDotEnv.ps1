248b23c65080e42cfa3f99a509b1acadf6cb9d65
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2021-07-07 23:58:54 +0700

    deploy to VM instead of DO app

---
+++ scripts/GenerateDotEnv.ps1
@@ -0,0 +1,30 @@
+#Requires -RunAsAdministrator
+
+Add-Type -AssemblyName 'System.Web'
+. ./GenerateClientSecret.ps1
+. ./GenerateCertificate.ps1
+
+$postgresPassword = [System.Web.Security.Membership]::GeneratePassword(14, 0)
+
+($WEB_CLIENT_KEY, $WEB_CLIENT_SECRET) = GenerateClientSecret
+($MOBILE_CLIENT_KEY, $MOBILE_CLIENT_SECRET) = GenerateClientSecret
+
+$IdentityServer_Password = [System.Web.Security.Membership]::GeneratePassword(14, 0)
+$IdentityServer_CertificateBytes = GenerateCertificateAsByteArray($IdentityServer_Password)
+$IdentityServer_Base64Certificate = [Convert]::ToBase64String($IdentityServer_CertificateBytes)
+
+$template = Get-Content -path './.env_template' -Raw
+
+$newContent	= $template + `
+'WEB_CLIENT_KEY='+$WEB_CLIENT_KEY+'
+WEB_CLIENT_SECRET='+$WEB_CLIENT_SECRET+'
+MOBILE_CLIENT_SECRET='+$MOBILE_CLIENT_SECRET+'
+POSTGRESS_PASSWORD='+$postgresPassword+'
+IdentityServer_Base64Certificate='+$IdentityServer_Base64Certificate+'
+IdentityServer_Password='+$IdentityServer_Password+'
+'
+
+
+$newContent | Set-Content -Path './.env.base'
+
+Write-Host 'Mobile Client Secret: ', $MOBILE_CLIENT_SECRET
