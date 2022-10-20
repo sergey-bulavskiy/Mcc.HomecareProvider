f346cec5cf3e5961e5822ab18a2eff6cf5c8b47a
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-10-20 11:16:22 +0700

    create `reader` user

---
+++ src/Mcc.HomecareProvider.Persistence/Migrations/20221020041528_CreateReaderUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mcc.HomecareProvider.Persistence.Migrations
+{
+    public partial class CreateReaderUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql(@"DO
+$do$
+BEGIN
+IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'reader') THEN
+
+      RAISE NOTICE 'Role ""reader"" already exists. Skipping.';
+   ELSE
+      CREATE ROLE reader LOGIN PASSWORD 'y6ecm2w9vui79uft';
+   END IF;
+   GRANT SELECT ON ALL TABLES IN SCHEMA public to reader;
+END
+$do$;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
