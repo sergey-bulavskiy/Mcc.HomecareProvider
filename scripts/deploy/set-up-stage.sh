44b8480a22decf3731bed50f5db4ea74d416a574
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-05-14 12:45:05 +0700

    Deploy to k3s

---
+++ scripts/deploy/set-up-stage.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# You could put here any set up code (e.g. creating directories, setting permissions, etc.)
+#
+mkdir ./deafult_file_storage -p
+chmod 777 ./deafult_file_storage
